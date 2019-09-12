472e2a7a588e42386d73c668ada61a482af5f986
“kylemharman” <kylemartinharman@gmail.com>
2019-09-13 07:44:16 +1000

    commiting changes berfore adding animations

--- script.js
+++ script.js
@@ -13,19 +13,24 @@ function gameBoard() {
 // respond to the player clicking a position. Check for winning combination after every play. 
 function respondToGameBoard() {
     for (let i = 0; i < getBoardPosition.length; i++) {
-        getBoardPosition[i].addEventListener('click', playersTurn); // find alternative to once. , { once: true }
+        getBoardPosition[i].addEventListener('click', playersTurn);
     }
+    document.getElementById('restartGame').addEventListener('click', resetGameBoard);
 }
 
 // track players turn and adds either X or O to game board board.
 function playersTurn() {
-    if (this.textContent === '') {     
+    if (this.textContent === '') {
         if (trackPlayersTurn % 2 === 0) {
             this.textContent = 'X';
             trackPlayersTurn++
+            playerOne.style.boxShadow = '0 4px 8px 0 rgba(82, 113, 255, 0.8)';
+            playerTwo.style.boxShadow = 'none';
         } else {
             this.textContent = 'O';
             trackPlayersTurn++;
+            playerTwo.style.boxShadow = '0 4px 8px 0 rgba(82, 113, 255, 0.8)';
+            playerOne.style.boxShadow = 'none';
         };
         updateGameBoard();
         findWinner();
@@ -41,6 +46,7 @@ function updateGameBoard() {
             board[i] = false;
         }
     }
+    highlightPlayer();
 }
 
 function findWinner() {
@@ -58,46 +64,76 @@ function findWinner() {
 }
 
 function announceWinner() {
-    const playerOne = "Player One";
-    const playerTwo = "Player Two";
+    const playerOne = "X WINS";
+    const playerTwo = "O WINS";
     const playerOneCurrentScore = document.getElementById('playerOneScore');
     const playerTwoCurrentScore = document.getElementById('playerTwoScore');
 
     if (trackPlayersTurn % 2 !== 0) {
         playerOneScore++;
         playerOneCurrentScore.textContent = playerOneScore;
-        resultsScreen(playerOne);
+        resultsOverlay(playerOne)
     } else {
         playerTwoScore++;
         playerTwoCurrentScore.textContent = playerTwoScore;
-        resultsScreen(playerTwo);
+        resultsOverlay(playerTwo)
     }
-    gameBoard();
     resetGameBoard();
 }
 
 function draw() {
+    const gameTied = 'DRAW';
     if (board.includes('') === false) {
-        alert(`Draw`);
-        gameBoard();
+        resultsOverlay(gameTied);
         resetGameBoard();
     }
 }
 
-function resultsScreen(player) {
-    alert(`${player} Wins`)
-};
+// Overlay to annouce who won the game or if the game was a tie. 
+function resultsOverlay(result) {
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = 'flex';
+    document.getElementById('resultOverlayText').innerText = result;
+    overlay.addEventListener('click', turnResultsOverlayOff);
+}
+
+function turnResultsOverlayOff() {
+    this.style.display = 'none';
+}
 
 function resetGameBoard() {
     for (let i = 0; i < getBoardPosition.length; i++) {
         getBoardPosition[i].textContent = '';
     }
+    gameBoard();
 }
 
 function highlightPlayer() {
-    const playerOne = document.getElementById('playerOne');
-    const playerTwo = document.getElementById('playerTwo');
+    const playerOne = document.querySelector('.playerHighlightOne');
+    const playerTwo = document.querySelector('.playerHighlightTwo');
+    if (trackPlayersTurn % 2 === 0) {
+        playerOne.style.boxShadow = '0 4px 8px 0 rgba(82, 113, 255, 0.8)';
+        playerTwo.style.boxShadow = 'none';
+    } else {
+        playerTwo.style.boxShadow = '0 4px 8px 0 rgba(82, 113, 255, 0.8)';
+        playerOne.style.boxShadow = 'none';
+    };
+}
+
+function animateCSS(element, animationName, callback) {
+    const animate = document.querySelector(element)
+    animate.classList.add('animated', animationName)
+
+    function handleAnimationEnd() {
+        animate.classList.remove('animated', animationName)
+        animate.removeEventListener('animationend', handleAnimationEnd)
+
+        if (typeof callback === 'function') callback()
+    }
+    
+    animate.addEventListener('animationend', handleAnimationEnd)
 }
 
 gameBoard();
+highlightPlayer()
 respondToGameBoard();
